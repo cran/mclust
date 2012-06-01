ea84ec01c8adcd1cf0335c39ead2ab68f0254304
Chris Fraley <fraley@stat.washington.edu>
2012-06-01 00:00:00 +0000

    version 3.4.11

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,3 @@
-.First.lib <- function(lib, pkg) {
-cat("\nby using mclust, invoked on its own or through another package,\n")
-cat("you accept the license agreement in the mclust LICENSE file\n")
-cat("and at http://www.stat.washington.edu/mclust/license.txt\n\n")
-  library.dynam("mclust", pkg, lib)
+.onAttach <- function(libname, pkgname){
+  packageStartupMessage("by using mclust, invoked on its own or through another package,\nyou accept the license agreement in the mclust LICENSE file\nand at http://www.stat.washington.edu/mclust/license.txt\n\n")
 }
